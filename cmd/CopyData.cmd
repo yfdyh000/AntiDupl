6542067e7d580d90ba3c2309249d8a3676ebee4a
YFdyh000 <yfdyh000@gmail.com>
2019-10-06 10:16:53 +0800

    3rd/Simd to git submodule
    added an update helper script for check out the latest commit

--- cmd/CopyData.cmd
+++ cmd/CopyData.cmd
@@ -2,15 +2,5 @@
 set SRC_DIR=%1
 set OUT_DIR=%2
 
-if exist %OUT_DIR%\data (
-echo Delete old data files:
-erase %OUT_DIR%\data\* /q /s /f
-rmdir %OUT_DIR%\data /q /s
-)
-
-echo Copy new data files:
-mkdir %OUT_DIR%\data
-mkdir %OUT_DIR%\data\resources
-
-xcopy %SRC_DIR%\data\resources\* %OUT_DIR%\data\resources\* /y /i /s /e
-xcopy %SRC_DIR%\docs\data\resources\* %OUT_DIR%\data\resources\* /y /i /s /e
+robocopy "%SRC_DIR%\data\resources" "%OUT_DIR%\data\resources" /MIR
+robocopy "%SRC_DIR%\docs\data\resources" "%OUT_DIR%\data\resources" /S
